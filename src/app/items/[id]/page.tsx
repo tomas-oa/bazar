9465f535e95a1b2c40f9239d058e91d7870feb19
Tomás Opazo <tomas.opazoasd@gmail.com>
2023-09-10 19:02:36 -0300

    fixed images container

---
+++ src/app/items/[id]/page.tsx
@@ -0,0 +1,5 @@
+export default function Item () {
+  return (
+    <h1>wenas</h1>
+  )
+}
--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -1,17 +1,22 @@
 import { type ProductI } from "@/types";
+import Link from "next/link";
 
 export default function Product ({product}: { product: ProductI}) {
   return (
-    <article className='grid grid-cols-[40%,1fr] gap-6'>
-      <img className='rounded-full' src={product.thumbnail} alt={product.title} />
-      <aside className='flex flex-col gap-1'>
-        <h1 className='text-xl font-bold'>{product.title}</h1>
-        <p className='text-sm'>{product.description}</p>
-        <div className='flex justify-between'>
-          <h1 className='text-md font-bold'>{product.price}$</h1>
-          <span>{product.rating }</span>
-        </div>
-      </aside>
-    </article>
+    <Link href={`items/${product.id}`}>
+      <article className='grid grid-cols-[45%,1fr] gap-6 p-2'>
+        <picture className="grid place-content-center">
+          <img width={'120px'} height={'120px'} className='object-cover object-center aspect-square border border-slate-200 rounded-full shadow-md' src={product.thumbnail} alt={product.title} />
+        </picture>
+        <aside className='flex flex-col gap-1'>
+          <h1 className='text-xl font-bold capitalize [text-wrap:balance]'>{product.title}</h1>
+          <p className='text-sm [text-wrap:balance]'>{product.description}</p>
+          <div className='flex justify-between'>
+            <h1 className='text-md font-bold'>{product.price}$</h1>
+            <span>{product.rating}</span>
+          </div>
+        </aside>
+      </article>
+    </Link>
   )
 }
