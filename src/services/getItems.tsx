fa21d71a063574709801bcb9aa54e478ac66fe42
Tomás Opazo <tomas.opazoasd@gmail.com>
2023-09-11 20:16:25 -0300

    no cache on api requests

--- src/services/getItems.tsx
+++ src/services/getItems.tsx
@@ -6,7 +6,7 @@ type Search = {
 
 
 export const getItems = async ({ search }: Search): Promise<ProductsI> => {
-  const data = await fetch(`http://localhost:3000/api/items?q=${search}`, { cache: 'reload' })
+  const data = await fetch(`http://localhost:3000/api/items?q=${search}`, { cache: 'no-cache' })
   const res = data.json()
 
   return res
