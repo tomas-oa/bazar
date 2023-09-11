bc527d9469b7c26c2269fc985be686b7a3ddc864
Tomás Opazo <tomas.opazoasd@gmail.com>
2023-09-11 20:17:19 -0300

    fix screen height

--- src/app/items/loading.tsx
+++ src/app/items/loading.tsx
@@ -2,7 +2,7 @@ import Spinner from "@/components/Spinner"
 
 export default function SearchLoading () {
   return (
-    <div className="flex flex-col items-center justify-center mt-24 gap-4">
+    <div className="flex flex-col items-center justify-center min-h-[600px] gap-4">
       <Spinner />
       <h5 className="font-semibold text-lg">Cargando...</h5>
     </div>
