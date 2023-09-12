b3790d277c7cadd40c119aa65bfdccc65872007f
Tomás Opazo <tomas.opazoasd@gmail.com>
2023-09-12 00:28:43 -0300

    fixed fetch url path

--- src/services/getItemDetails.ts
+++ src/services/getItemDetails.ts
@@ -5,7 +5,7 @@ interface Id {
 }
 
 export const getItemsDetails = async ({ id }: Id): Promise<ProductI> => {
-  const data = await fetch(`https://${process.env.NEXT_PUBLIC_API_URL}/api/items/${id}`, { method: 'GET', cache: "no-cache" })
+  const data = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/items/${id}`, { method: 'GET', cache: "no-cache" })
   const res = data.json()
 
   return res
