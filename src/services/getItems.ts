bce5744f8187eed2f829c988b093a2bad27d494b
Tomás Opazo <tomas.opazoasd@gmail.com>
2023-09-11 23:17:03 -0300

    api url fix

--- src/services/getItems.ts
+++ src/services/getItems.ts
@@ -6,7 +6,7 @@ type Search = {
 
 
 export const getItems = async ({ search }: Search): Promise<ProductsI> => {
-  const data = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/items?q=${search}`, { cache: 'no-cache' })
+  const data = await fetch(`https://${process.env.NEXT_PUBLIC_API_URL}/api/items?q=${search}`, { cache: 'no-cache' })
   const res = data.json()
 
   return res
