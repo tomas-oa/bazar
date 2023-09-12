f5d7bd8069735c4282795df12f834a0fcffdb908
Tomás Opazo <tomas.opazoasd@gmail.com>
2023-09-12 14:35:05 -0300

    category width fixed

--- src/components/Category.tsx
+++ src/components/Category.tsx
@@ -6,7 +6,7 @@ interface Props {
 
 export default function Category ({ category }: Props) {
   return (
-    <Link href={`/items?search=${category.name}`}>
+    <Link className="max-w-[100px]" href={`/items?search=${category.name}`}>
       <li className='bg-pink-300 text-center min-w-[100px] px-2 py-1.5 rounded-sm text-xs truncate'>{category.name} - {category.count}</li>
     </Link>
   )
