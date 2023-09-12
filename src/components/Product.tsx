c43f086d1117df9db30b8fe2f1cfa72eeaf76e32
Tomás Opazo <tomas.opazoasd@gmail.com>
2023-09-12 14:35:51 -0300

    lazy loading on products images

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -7,16 +7,14 @@ export default function Product ({product}: { product: ProductI}) {
     <Link href={`items/${product.id}`}>
       <article className='grid grid-cols-[45%,1fr] gap-6 p-2'>
         <picture className="grid place-content-center">
-          <img width={'120px'} height={'120px'} className='object-cover object-center aspect-square border border-slate-200 rounded-full shadow-md' src={product.thumbnail} alt={product.title} />
+          <img loading="lazy" width={'120px'} height={'120px'} className='object-cover object-center aspect-square border border-slate-200 rounded-full shadow-md' src={product.thumbnail} alt={product.title} />
         </picture>
-        <aside className='flex flex-col gap-1'>
+        <aside className='flex flex-col justify-between'>
           <h1 className='text-xl font-bold capitalize [text-wrap:balance]'>{product.title}</h1>
           <p className='text-sm [text-wrap:balance]'>{product.description}</p>
-          <div className='flex justify-between items-center'>
+          <div className='flex justify-between items-center gap-2'>
             <h2 className='text-md text-center font-bold h-full flex items-center justify-center pt-1'>
-              <span>
-                {product.price}$
-              </span>
+              <span>{product.price}$</span>
             </h2>
             <Rating rating={product.rating} />
           </div>
